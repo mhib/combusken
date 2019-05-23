8041fab22f8680e1b2195002d1857914b7b1f199
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2019-05-23 11:40:52 +0200

    Improve pgn_to_fen script

--- evaluation/pgn_to_fen.rb
+++ evaluation/pgn_to_fen.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 require 'parallel'
-require 'thread'
 require 'pgn'
 
 @q = Queue.new
-reader = Thread.new do |n|
+reader = Thread.new do
   acc = +''
-  IO.foreach("./games.pgn") do |line|
+  IO.foreach('./games.pgn') do |line|
     if !acc.empty? && line.include?('Event')
       @q << acc
       acc = +line
@@ -25,10 +24,10 @@ end
 Parallel.each(@q, in_processes: 7) do |pgn|
   game = PGN.parse(pgn).first
   game.moves.each_with_index do |move, idx|
-    if move.comment.include? 'M'
-      break
-    end
-    @result.write(game.positions[idx + 1].to_fen.to_s + ";#{game.result}\n")
+    next if idx.zero? # do not include position from book
+    break if move.comment.include? 'M' # stop if mate was found in position
+
+    @result.write(game.positions[idx].to_fen.to_s + ";#{game.result}\n")
     @result.flush
   end
 end
