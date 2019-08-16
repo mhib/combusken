313bdd6a9c60bb6e583758b8a6b9024a6c9dfe45
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2019-08-17 00:14:19 +0200

    Use standard library bitscan

--- backend/bitboard.go
+++ backend/bitboard.go
@@ -245,21 +245,8 @@ var (
 	KnightAttacks, KingAttacks         [64]uint64
 )
 
-var index64 = [64]int{0, 47, 1, 56, 48, 27, 2, 60,
-	57, 49, 41, 37, 28, 16, 3, 61,
-	54, 58, 35, 52, 50, 42, 21, 44,
-	38, 32, 29, 23, 17, 11, 4, 62,
-	46, 55, 26, 59, 40, 36, 15, 53,
-	34, 51, 20, 43, 31, 22, 10, 45,
-	25, 39, 14, 33, 19, 30, 9, 24,
-	13, 18, 8, 12, 7, 6, 5, 63,
-}
-
-const debruijn64 uint64 = 0x03f79d71b4cb0a89
-
-// https://www.chessprogramming.org/index.php?title=BitScan
 func BitScan(bb uint64) int {
-	return index64[((bb^(bb-1))*debruijn64)>>58]
+	return bits.TrailingZeros64(bb)
 }
 
 func MoreThanOne(bb uint64) bool {
