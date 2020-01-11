e93dbb867454564112d3e705cc467fbc38e4471e
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2020-01-11 23:43:12 +0100

    Retuning and scale

--- tools/scale_eval_values_to_cp.rb
+++ tools/scale_eval_values_to_cp.rb
@@ -1,8 +1,8 @@
 #!/usr/local/bin/ruby
 
-INITAL_PAWN_VALUE = 173
-SCALE = 100.0 / INITAL_PAWN_VALUE
-REGEXP = /Score{(-?\d+, -?\d+)}/
+INITAL_PAWN_VALUE = 69r
+SCALE = 100 / INITAL_PAWN_VALUE
+REGEXP = /S\((-?\d+, -?\d+)\)/
 
 EVAL_PATH = File.join(__dir__, '..', 'evaluation', 'eval.go')
 
@@ -10,7 +10,7 @@ eval_content = IO.read(EVAL_PATH)
 
 new_eval = eval_content.gsub(REGEXP) do |match|
   middle_score, end_score = match.scan(/-?\d+/).map(&:to_i)
-  "Score{#{(middle_score * SCALE).ceil}, #{(end_score * SCALE).ceil}}"
+  "S(#{(middle_score * SCALE).ceil}, #{(end_score * SCALE).ceil})"
 end
 
 IO.write(EVAL_PATH, new_eval, mode: 'w')
