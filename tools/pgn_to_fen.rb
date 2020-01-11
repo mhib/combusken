16356506a2a0a704094469c87346da907ad05edb
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2020-01-11 20:46:45 +0100

    Retuning tools

--- tools/pgn_to_fen.rb
+++ tools/pgn_to_fen.rb
@@ -7,7 +7,7 @@ require 'pgn'
 reader = Thread.new do
   acc = +''
   IO.foreach('./games.pgn') do |line|
-    next if line.start_with?('{')
+    next if line.start_with?('{White')
     if !acc.empty? && line.include?('Event')
       @q << acc
       acc = +line
@@ -23,11 +23,19 @@ end
 @result = File.new('./games.fen', 'a')
 
 Parallel.each(@q, in_processes: 7) do |pgn|
-  game = PGN.parse(pgn).first
+  game = nil
+  begin
+    game = PGN.parse(pgn).first
+  rescue Exception => e
+    p e
+    puts pgn
+    puts "---"
+    next
+  end
   game.moves.each_with_index do |move, idx|
     break if move.comment.nil?
-    next if idx.zero? || move.comment.include?('book')# do not include position from book
-    break if move.comment.include? 'M' # stop if mate was found in position
+    next if idx.zero? || move.comment.include?('book') || move.comment.include?('+0.00/1')# do not include position from book
+    break if move.comment.include?('M') || move.comment.include?('#') # stop if mate was found in position
 
     @result.write(game.positions[idx].to_fen.to_s + ";#{game.result}\n")
     @result.flush
