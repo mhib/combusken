fd445314fec200c0d9090e9fcbb81982c083f828
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2020-04-27 20:26:42 +0200

    Counter move history pruning

--- engine/search.go
+++ engine/search.go
@@ -28,6 +28,7 @@ const reverseFutilityPruningMargin = 90
 
 const moveCountPruningDepth = 8
 const futilityPruningDepth = 8
+const counterMovePruningDepth = 3
 
 const probCutDepth = 6
 const probCutMargin = 100
@@ -331,6 +332,9 @@ func (t *thread) alphaBeta(depth, alpha, beta, height int, inCheck bool) int {
 				continue
 			}
 			if depth <= moveCountPruningDepth && moveCount >= moveCountPruning(BoolToInt(height <= 2 || t.stack[height].Evaluation() >= t.stack[height-2].Evaluation()), depth) {
+				continue
+			}
+			if depth <= counterMovePruningDepth && pos.LastMove != NullMove && t.CounterHistoryValue(pos.LastMove, move) < 0 {
 				continue
 			}
 		}
