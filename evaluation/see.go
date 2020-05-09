55388d1b3a2565c7e6ebc08432304a2b8fe62404
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2020-05-09 11:11:24 +0200

    Move see values back to see

--- evaluation/see.go
+++ evaluation/see.go
@@ -3,6 +3,8 @@ package evaluation
 import . "github.com/mhib/combusken/backend"
 import . "github.com/mhib/combusken/utils"
 
+var SEEValues = [...]int{100, 450, 450, 675, 1300, Mate / 2, 0}
+
 // Returns true if see non-negative
 func SeeSign(pos *Position, move Move) bool {
 	return SeeAbove(pos, move, 0)
