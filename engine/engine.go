465b71246e61c7c3a6b3f6ed91cbc81948bde11f
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2020-05-19 19:12:27 +0200

    Bump version

--- engine/engine.go
+++ engine/engine.go
@@ -111,7 +111,7 @@ type SearchParams struct {
 }
 
 func (e *Engine) GetInfo() (name, version, author string) {
-	return "Combusken", "1.1.1", "Marcin Henryk Bartkowiak"
+	return "Combusken", "1.2.0", "Marcin Henryk Bartkowiak"
 }
 
 func (e *Engine) GetOptions() []EngineOption {
