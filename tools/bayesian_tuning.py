44870d36a0a679eba8ebae7f59b54f3f8f064e49
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2020-07-05 13:01:41 +0200

    Minor improvements

--- tools/bayesian_tuning.py
+++ tools/bayesian_tuning.py
@@ -1,6 +1,6 @@
 from skopt import gp_minimize
 from tempfile import NamedTemporaryFile
-from os import path, chdir, system, pipe, read, remove
+from os import chdir, system, pipe, remove, fdopen, close
 from shutil import copy
 from typing import List
 import subprocess
@@ -36,14 +36,18 @@ def replace_value(parameter: str, value: int):
                 new_file.write(line)
         new_file.flush()
         copy(new_file.name, '../engine/search.go')
-        
 
 def get_current_value(pattern):
     for _i, line in enumerate(open('../engine/search.go')):
         for match in re.finditer(pattern, line):
             return int(match.group(1))
 
+
+elo_regex = re.compile(r'Elo difference: (-?\d+\.?\d*)')
+inf_regex = re.compile(r'Elo difference: (-?inf)')
 def calculate_elo_diff(parameters: List[SearchConstant], values: List[int]):
+    calculate_elo_diff.call_count += 1
+    print("{} call {}".format(calculate_elo_diff.call_count, values))
     binary_name = '-'.join(["combusken"] + [p.name for p in parameters] + list(map(str, values)))
     for parameter, value in zip(parameters, values):
         replace_value(parameter.name, value)
@@ -53,37 +57,39 @@ def calculate_elo_diff(parameters: List[SearchConstant], values: List[int]):
         cute_chess_command('../combusken', '../' + binary_name).split(),
         stdout=pipe_write
     )
-    elo_regex = re.compile(r'Elo difference: (-?\d+\.?\d*)')
-    inf_regex = re.compile(r'Elo difference: (-?inf)')
-    while True:
-        line = read(pipe_read, 1000).strip().decode('ascii')
-        print(line)
-        if line.startswith('Elo difference'):
-            process.kill()
-            remove('../' + binary_name)
+    close(pipe_write)
+    with fdopen(pipe_read) as fd:
+        while True:
+            line = fd.readline().strip()
+            print(line)
+            if line.startswith('Elo difference'):
+                process.kill()
+                remove('../' + binary_name)
 
-            if inf_regex.match(line):
-                match = inf_regex.match(line)
-                return float(match.group(1))
+                if inf_regex.match(line):
+                    match = inf_regex.match(line)
+                    return float(match.group(1))
 
-            if elo_regex.match(line):
-                match = elo_regex.match(line)
-                return float(match.group(1))
+                if elo_regex.match(line):
+                    match = elo_regex.match(line)
+                    return float(match.group(1))
 
 def build_engine(exe_name):
     chdir('..')
     system('go build -o {} combusken.go'.format(exe_name))
     chdir('./tools')
-    
+
 
 def optimize_parameter(parameters: List[SearchConstant]):
+    calculate_elo_diff.call_count = 0
     build_engine('combusken')
     current_values = [get_current_value(const_pattern(x.name)) for x in parameters]
     res = gp_minimize(
         lambda xs: calculate_elo_diff(parameters, list(map(round, xs))),
         [(value - parameter.range, value + parameter.range) for value, parameter in zip(current_values, parameters)],
         n_calls=100,
-        n_random_starts=5
+        n_random_starts=5,
+        acq_func="EI",
     )
     print(res)
 
