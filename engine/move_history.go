f38742e657b9b536257d7196cd35854d6ef021e0
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2020-09-26 21:31:18 +0200

    Tune history

--- engine/move_history.go
+++ engine/move_history.go
@@ -10,9 +10,9 @@ import (
 const MinSpecialMoveValue = 53000
 const MaxBadCapture = -100000 + 4096 // 4096 represents max mvvlva value
 
-const HistoryMax = 400
-const HistoryMultiplier = 32
-const HistoryDivisor = 512
+const HistoryMax = 397
+const HistoryMultiplier = 47
+const HistoryDivisor = 482
 
 type MoveHistory struct {
 	KillerMoves      [STACK_SIZE + 1][2]Move
