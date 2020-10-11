e5cf2b3b20ac53be04c4b3b6de37ec2dee24191f
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2020-10-11 15:59:24 +0200

    Change tt replacement formula

--- transposition/transposition_table.go
+++ transposition/transposition_table.go
@@ -98,8 +98,8 @@ func (t *TranspositionTable) Set(key uint64, value int16, eval int16, depth int,
 		element = &bucket[1]
 	} else {
 		element = &bucket[0]
-		firstScore := (int(element.ageFlag.Age()) - int(t.Age)) + (int(element.depth)-depth)*2
-		secondScore := (int(bucket[1].ageFlag.Age()) - int(t.Age)) + (int(bucket[1].depth)-depth)*2
+		firstScore := (int(element.ageFlag.Age()) - int(t.Age)) + (int(element.depth)-depth)*4
+		secondScore := (int(bucket[1].ageFlag.Age()) - int(t.Age)) + (int(bucket[1].depth)-depth)*4
 		if firstScore > 0 && secondScore > 0 {
 			return
 		}
